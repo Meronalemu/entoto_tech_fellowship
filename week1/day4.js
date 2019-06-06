b6c2c43d851354d0c543dd6ee797d0fabff3bdae
Meronalemu <meron2304@gmail.com>
2019-06-06 00:34:26 -0700

    creating objects and using array

---
+++ week1/day4.js
@@ -0,0 +1,22 @@
+///creating an obg=ject //
+var book= {
+      title : "MERBEBT",
+      author : "Dr Alemayew Wsea",
+      releaseDate: "feb 2,2011"
+  
+   } 
+console.log(book) 
+console.log(title) 
+console.log(book.author)
+console.log(book.releaseDate)
+////creating an array//
+var food = ["rice","burger","shero"]
+for ( i=0; i<food.length; i++)
+ {
+   console.log(food[i]) 
+ }
+///counting from 1 to 10//
+var count=1;
+while (count <10) {
+  console.log(count++)
+}
