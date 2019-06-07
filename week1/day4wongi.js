1582b0239ddb84d65733c98db178629c5f21ec5c
Meronalemu <meron2304@gmail.com>
2019-06-07 00:00:12 -0700

    listing numbers

---
+++ week1/day4wongi.js
@@ -0,0 +1,8 @@
+//list a number and state it even or odd
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+    console.log(i + ' is even number');
+    } else {
+    console.log(i + ' is odd number');
+    }
+}
